c3c6e6b02d48c75ca3eabbe4108b71316dcad002
Aamir <amirkamalov2006@gmail.com>
2025-04-15 00:00:00 +0500

    Final

---
+++ sample.js
@@ -0,0 +1,23 @@
+const gnar = "gnarly";
+
+const info = ({
+  file = __filename,
+  dir = __dirname
+}) => (
+  <p>{dir}: {file}</p>
+);
+
+switch (gnar) {
+  default:
+    console.log("gnarly");
+    break;
+}
+
+function gnarHook() {
+  const [nickname, setNickname] = useState("dude");
+  return <h1>gnarly</h1>;
+}
+
+function Image() {
+  return <img src="/img.png" />;
+}
